281097a52a3673cf83e5601cb4b3b013861d2130
lemonxixixi <43404481+lemonxixixi@users.noreply.github.com>
2019-02-05 12:56:59 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,32 @@
-function preload(){
-  // put preload code here
-}
+let capture;
 
 function setup() {
-  // put setup code here
+	createCanvas(windowWidth, windowHeight);
+	background(100);
+
+	capture = createCapture(VIDEO);
+	capture.size(640,480);　　
+	capture.hide();
+
+	background(0);
 }
 
 function draw() {
-  // put drawing code here
+
+	let ratioW = capture.width/width;
+	let ratioH = capture.height/height;
+
+
+	image(capture, 0, 0, 160, 120);
+	capture.loadPixels();
+
+
+	for(let i = 0; i < 2000; i++){
+	let x = int(random(capture.width));
+	let y = int(random(capture.height));
+	let col = capture.get(x,y);
+	fill (col);
+	noStroke();
+	ellipse(x/ratioW,y/ratioH,random(0,25));
+	}
 }
